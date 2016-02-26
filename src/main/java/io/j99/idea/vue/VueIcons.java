3ca10e276d218a6c85fea4eb3ba1164172cba3ad
henjue <henjue@gmail.com>
2016-02-26 08:42:30 +0800

    remote kotlin code

---
+++ src/main/java/io/j99/idea/vue/VueIcons.java
@@ -0,0 +1,13 @@
+package io.j99.idea.vue;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Created by apple on 16/2/26.
+ */
+public class VueIcons {
+    private static final  String PATH="/io/j99/idea/vue/icons/";
+    public static Icon VUE_ICON = IconLoader.getIcon(PATH+ "logo.png");
+}
--- src/main/kotlin/io/j99/idea/vue/highlighter/VueSyntaxHighlighter.kt
+++
@@ -1,7 +0,0 @@
-package io.j99.idea.vue.highlighter
-
-/**
- * Created by android on 15-12-25.
- */
-class VueSyntaxHighlighter : com.intellij.ide.highlighter.HtmlFileHighlighter() {
-}
